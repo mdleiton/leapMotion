b97dc98136d50d4f7f8bc7d385c0479e377bf746
mdleiton <mau_leiton96@hotmail.com>
2019-08-23 20:23:32 -0500

    [add] new function to resize

--- LeapSDK_mauricio/leap_motion/src/dataset_test.py
+++ LeapSDK_mauricio/leap_motion/src/dataset_test.py
@@ -5,45 +5,41 @@ from PIL import Image
 import pyautogui
 import test as ts
 
-mnist = tf.keras.datasets.mnist
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
 imagen = 'screenshot1.png'
 arrayimg=np.array(ts.convertirImagen(imagen))#imagen se convierte a un arreglo como estan en mnist
 
 arrayimg_norm = np.array([tf.keras.utils.normalize(arrayimg,axis=1)]) #arreglo de la imagen entre 0 y 1, mas eficiente y rapido para el tensorflow
-#plt.imshow(arrayimg)
-#plt.show()
-
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
-x_test7 = np.array([x_test[84]])
-#print(np.where(y_test == 8))
-#print(x_test.shape)
-#print(arrayimg_norm.shape)
-#print(x_test7)
-plt.imshow(x_test7[0],cmap=plt.cm.binary)
+plt.imshow(arrayimg_norm[0],cmap=plt.cm.binary)
 plt.show()
+
 #modelo que va a entrenar
+MODEL_FILE = True
+model = None
+if MODEL_FILE:
+    model = tf.keras.models.load_model('modelo_erick/modelo_test.model') #abro mi modelo que aprendio
+else:
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train),(x_test, y_test) = mnist.load_data()
+    x_train = tf.keras.utils.normalize(x_train, axis=1)
+    x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(10,activation=tf.nn.softmax))
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(10,activation=tf.nn.softmax))
 
-model.compile(optimizer='adam',
-            loss='sparse_categorical_crossentropy',
-            metrics=['accuracy'])
+    model.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
 
-model.fit(x_train,y_train,epochs=5)
-model.save('modelo_erick/modelo_test.model') #guardamos el modelo que entreno
-
-new_model = tf.keras.models.load_model('modelo_erick/modelo_test.model') #abro mi modelo que aprendio
-#load_data
-val_loss, val_acc = new_model.evaluate(x_test, y_test)
-#print(val_acc)
+    model.fit(x_train,y_train,epochs=5)
+    model.save('modelo_erick/modelo_test.model') #guardamos el modelo que entreno
+    #load_data
+    val_loss, val_acc = model.evaluate(x_test, y_test)
+    print(val_acc)
 
 #prediction = new_model.predict(x_test7)[0] #obtengo el primer y unico elemento porque solo estoy prediciendo una imagen
-prediction = new_model.predict(arrayimg_norm)[0]
+prediction = model.predict(arrayimg_norm)[0]
 print("Prediccion: %d"%np.argmax(prediction))
 #print(predictions)
--- LeapSDK_mauricio/leap_motion/src/test.py
+++ LeapSDK_mauricio/leap_motion/src/test.py
@@ -3,68 +3,62 @@ from matplotlib import pyplot as plt
 
 
 def imageprepare(argv):
-    """
-    This function returns the pixel values.
-    The imput is a png file location.
-    """
-    im = Image.open(argv).convert('L')
-    width = float(im.size[0])
-    height = float(im.size[1])
-    newImage = Image.new('L', (28, 28), (255))  # creates white canvas of 28x28 pixels
+	"""
+	This function returns the pixel values.
+	The imput is a png file location.
+	"""
+	im = Image.open(argv).convert('L')
+	width = float(im.size[0])
+	height = float(im.size[1])
+	newImage = Image.new('L', (28, 28), (255))  # creates white canvas of 28x28 pixels
 
-    if width > height:  # check which dimension is bigger
-        # Width is bigger. Width becomes 20 pixels.
-        nheight = int(round((20.0 / width * height), 0))  # resize height according to ratio width
-        if (nheight == 0):  # rare case but minimum is 1 pixel
-            nheight = 1
-            # resize and sharpen
-        img = im.resize((20, nheight), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
-        wtop = int(round(((28 - nheight) / 2), 0))  # calculate horizontal position
-        newImage.paste(img, (4, wtop))  # paste resized image on white canvas
-    else:
-        # Height is bigger. Heigth becomes 20 pixels.
-        nwidth = int(round((20.0 / height * width), 0))  # resize width according to ratio height
-        if (nwidth == 0):  # rare case but minimum is 1 pixel
-            nwidth = 1
-            # resize and sharpen
-        img = im.resize((nwidth, 20), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
-        wleft = int(round(((28 - nwidth) / 2), 0))  # caculate vertical pozition
-        newImage.paste(img, (wleft, 4))  # paste resized image on white canvas
+	if width > height:  # check which dimension is bigger
+		# Width is bigger. Width becomes 20 pixels.
+		nheight = int(round((20.0 / width * height), 0))  # resize height according to ratio width
+		if (nheight == 0):  # rare case but minimum is 1 pixel
+			nheight = 1
+		img = im.resize((20, nheight), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)  # resize and sharpen
+		wtop = int(round(((28 - nheight) / 2), 0))  # calculate horizontal position
+		newImage.paste(img, (4, wtop))  # paste resized image on white canvas
+	else:
+		# Height is bigger. Heigth becomes 20 pixels.
+		nwidth = int(round((20.0 / height * width), 0))  # resize width according to ratio height
+		if (nwidth == 0):  # rare case but minimum is 1 pixel
+			nwidth = 1
+		img = im.resize((nwidth, 20), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)  # resize and sharpen
+		wleft = int(round(((28 - nwidth) / 2), 0))  # caculate vertical pozition
+		newImage.paste(img, (wleft, 4))  # paste resized image on white canvas
+	tv = list(newImage.getdata())  # get pixel values
+	tva = [(255.0 - x) * 1.0 / 255.0 for x in tv]
+	return tva
 
-    # newImage.save("sample.png
 
-    tv = list(newImage.getdata())  # get pixel values
-
-    # normalize pixels to 0 and 1. 0 is pure white, 1 is pure black.
-    tva = [(255.0 - x) * 1.0 / 255.0 for x in tv]
-    #print(tva)
-    return tva
-
+def resizeImage(argv):
+	"""
+	This function resize the image to a 28x28.
+	The imput is a png file location.
+	"""
+	try:
+		size = (28,28)
+		im = Image.open(argv).convert('L')
+		im = im.resize(size, Image.NEAREST)
+		return list(im.getdata())
+	except IOError:
+		print "cannot resize image '%s'" % argv
+		return None
 
 
 def convertirImagen(imagefile):
-
-	x=[imageprepare(imagefile)]#file path here
-	#print(len(x))# mnist IMAGES are 28x28=784 pixels
-	#print(x[0])
-	#Now we convert 784 sized 1d array to 24x24 sized 2d array so that we can visualize it
-	newArr=[[0 for d in range(28)] for y in range(28)]
+	"""
+	This function resize the image to a 28x28.
+	The imput is a png file location.
+	"""
+	x = [resizeImage(imagefile)]		#file path here
+	# Now we convert 784 sized 1d array to 24x24 sized 2d array so that we can visualize it
+	newArr = [[0 for d in range(28)] for y in range(28)]
 	k = 0
 	for i in range(28):
-    		for j in range(28):
-        		newArr[i][j]=round(x[0][k]*100)
-        		k=k+1
+		for j in range(28):
+			newArr[i][j] = 255.0 - x[0][k]
+			k = k + 1
 	return newArr
-
-#for i in range(28):
-#    for j in range(28):
-#         print(round(newArr[i][j]),end=" ")
-        #print(' , ')
-#    print('\n')
-
-#print(newArr)
-#plt.imshow(newArr, interpolation='nearest')
-
-#plt.savefig('MNIST_IMAGE.png')#save MNIST image
-#plt.gray()
-#plt.show()#Show / plot that image
